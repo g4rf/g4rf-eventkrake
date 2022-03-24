8e9771ed8409c2b39323680bada6954ec8d10e19
garfield <garfield@garfield>
2022-03-24 07:12:35 +0100

    added widget for upcoming events (alpha)

--- g4rf-eventkrake3.php
+++ g4rf-eventkrake3.php
@@ -4,7 +4,7 @@ Plugin Name: Eventkrake 3 WP Plugin
 Plugin URI: http://eventkrake.de/
 Description: A wordpress plugin to manage events, locations and artists. It has an REST endpoint to use the data in external applications.
 Author: Jan Kossick
-Version: 3.5beta
+Version: 3.6beta
 License: CC BY-NC-SA 4.0, https://creativecommons.org/licenses/by-nc-sa/4.0/
 Author URI: http://jankossick.de
 Min WP Version: 5.3
@@ -111,6 +111,205 @@ add_action('wp_enqueue_scripts', function() {
 });
 
 
+/***** Widgets *****/
+
+class Eventkrake_ShowNextEvents extends WP_Widget {
+
+	/**
+	 * Sets up the widgets name etc
+	 */
+	public function __construct() {
+        parent::__construct(
+			'eventkrake_shownextevents', // id
+			esc_html__('Eventkrake :: Next Events', 'g4rf_eventkrake2'), // name
+			array( 
+                'description' => 
+                    esc_html__('Shows the upcoming events.', 'g4rf_eventkrake2')
+            ) // args
+		);
+	}
+
+	/**
+	 * Outputs the content of the widget
+	 *
+	 * @param array $args
+	 * @param array $instance
+	 */
+	public function widget($args, $instance) {
+        print $args['before_widget'];
+        
+        // title
+		if(! empty($instance['title'])) {
+			print $args['before_title'];
+            print apply_filters('widget_title', $instance['title']);
+            print $args['after_title'];
+		}
+        
+        // events
+        $data = json_decode( file_get_contents( get_site_url(null, 
+            '/wp-json/eventkrake/v3/events?earliestEnd=' . date('c'),
+            is_ssl() ? 'https' : 'http')));
+        
+        ?><events class="eventkrake"><?php
+        foreach($data->events as $event) { ?>
+        
+            <event class="<?=implode(' ', $event->categories)?> <?=$event->tags ?>">
+                <name><?=$event->name ?></name>
+                <text><?=$event->text ?></text>
+                <picture>
+                    <img src="<?=$event->image?>" alt="<?=$event->name?>" />
+                </picture>
+                <start><?=
+                    (new DateTime($event->start))
+                        ->format($instance['date_format_start']) 
+                ?></start>
+                <end><?=
+                    (new DateTime($event->end))
+                        ->format($instance['date_format_end']) 
+                ?></end>
+                <categories><?=
+                    implode(', ', $event->categories);
+                ?></categories>
+                <links><?php
+                    foreach($events->links as $link) { ?>
+                        <a href="<?=$link['url']?>"><?=$link['name']?></a>
+                    <?php } ?>
+                ?></links>
+                <tags><?=$event->tags ?></tags>
+                
+                <?php // location 
+                    $location = $data->locations[$event->locationid];
+                    ?><location class="<?=implode('  ', $location->categories);?>">
+                        <name><?=$location->name ?></name>
+                        <address><?=$location->address ?></address>
+                        <lat><?=$location->lat ?></lat>
+                        <lng><?=$location->lng ?></lng>
+                        <text><?=$location->text ?></text>
+                        <picture>
+                            <img src="<?=$location->image?>"
+                                 alt="<?=$location->name?>" />
+                        </picture>
+                        <categories><?=
+                            implode(', ', $location->categories);
+                        ?></categories>
+                        <links><?php
+                            foreach($location->links as $link) { ?>
+                                <a href="<?=$link['url']?>"><?=$link['name']?></a>
+                            <?php } ?>
+                        ?></links>
+                        <tags><?=$location->tags ?></tags>
+                    </location>
+                
+                <?php // artist
+                    foreach($event->artists as $artistId) {
+                        $artist = $data->artists[$artistId];
+                        ?><artist class="<?=implode('  ', $artist->categories);?>">
+                            <name><?=$artist->name ?></name>
+                            <text><?=$artist->text ?></text>
+                            <picture>
+                                <img src="<?=$artist->image?>"
+                                     alt="<?=$artist->name?>" />
+                            </picture>
+                            <categories><?=
+                                implode(', ', $artist->categories);
+                            ?></categories>
+                            <links><?php
+                                foreach($artist->links as $link) { ?>
+                                    <a href="<?=$link['url']?>"><?=$link['name']?></a>
+                                <?php } ?>
+                            ?></links>
+                            <tags><?=$artist->tags ?></tags>
+                        </artist>
+                <?php } ?>                  
+            </event>
+            
+        <?php } ?></events><?php
+        
+        // after widget
+		print $args['after_widget'];
+	}
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @see WP_Widget::form()
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form($instance) {
+		$title = $instance['title'];
+        $count = $instance['count'];
+        $dateFormatStart = $instance['date_format_start'];
+        $dateFormatEnd = $instance['date_format_end'];
+		?>
+        <!-- title -->
+		<p>
+            <label for="<?php echo esc_attr($this->get_field_id('title')); ?>">
+                <?php esc_attr_e('Title:', 'g4rf_eventkrake2'); ?></label> 
+            <input 
+                class="widefat" 
+                id="<?php echo esc_attr($this->get_field_id('title')); ?>" 
+                name="<?php echo esc_attr($this->get_field_name('title')); ?>" 
+                type="text" value="<?php echo esc_attr($title); ?>">
+		</p>
+        <!-- count -->
+        <p>
+            <label for="<?php echo esc_attr($this->get_field_id('count')); ?>">
+                <?php esc_attr_e('Number of events:', 'g4rf_eventkrake2'); ?></label> 
+            <input 
+                id="<?php echo esc_attr($this->get_field_id('count')); ?>" 
+                name="<?php echo esc_attr($this->get_field_name('count')); ?>" 
+                type="number" value="<?php echo esc_attr($count); ?>">
+		</p>
+        <!-- dateFormatStart -->
+        <p>
+            <label for="<?php echo esc_attr($this->get_field_id('date_format_start')); ?>">
+                <?php esc_attr_e('Date format for start date:', 'g4rf_eventkrake2'); ?></label> 
+            <input 
+                id="<?php echo esc_attr($this->get_field_id('date_format_start')); ?>" 
+                name="<?php echo esc_attr($this->get_field_name('date_format_start')); ?>" 
+                type="text" value="<?php echo esc_attr($dateFormatStart); ?>">
+		</p>
+        <!-- dateFormatEnd -->
+        <p>
+            <label for="<?php echo esc_attr($this->get_field_id('date_format_end')); ?>">
+                <?php esc_attr_e('Date format for end date:', 'g4rf_eventkrake2'); ?></label> 
+            <input 
+                id="<?php echo esc_attr($this->get_field_id('date_format_end')); ?>" 
+                name="<?php echo esc_attr($this->get_field_name('date_format_end')); ?>" 
+                type="text" value="<?php echo esc_attr($dateFormatEnd); ?>">
+		</p>
+    <?php }
+
+	/**
+	 * Processing widget options on save
+	 *
+	 * @param array $new_instance The new options
+	 * @param array $old_instance The previous options
+	 *
+	 * @return array
+	 */
+	public function update($new_instance, $old_instance) {
+		$instance = array();
+        
+        // title
+		$instance['title'] = '';
+        if(! empty($new_instance['title'])) {
+            $instance['title'] = sanitize_text_field($new_instance['title']);
+        }
+        
+        // number of events
+        $instance['count'] = 0;
+        if(! empty($new_instance['count'])) {
+            $instance['count'] = sanitize_text_field($new_instance['count']);
+        }
+
+		return $instance;
+	}
+}
+add_action('widgets_init', function() {
+    register_widget('Eventkrake_ShowNextEvents');
+});
 
 /***** Custom Post Types *****/
 
