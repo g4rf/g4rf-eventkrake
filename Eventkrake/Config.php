bfaa4bf76f4c2b9a9cddc1382054cc417dab1321
garfield <admin@g4rf.net>
2023-07-03 19:24:02 +0900

    added config class to put configs later on an admin screen

---
+++ Eventkrake/Config.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Eventkrake;
+
+/**
+ * TODO: Put it in an admin page.
+ */
+class Config {
+    public static function dateFormat() {
+        return 'd. MMM';
+    }
+    
+    public static function timeFormat() {
+        return "H:mm";
+    }
+}
